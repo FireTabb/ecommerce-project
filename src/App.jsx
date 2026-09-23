012b6ecb47304ef6e29413c96aee8762bed58d46
FireTabb <agh.ir.agh@gmail.com>
2026-09-23 17:00:04 +0330

    OrdersPage and TrackingPage is added from html to react

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { HomePage } from "./pages/HomePage";
-import {CheckoutPage} from "./pages/CheckoutPage"
+import { CheckoutPage } from "./pages/CheckoutPage";
+import { OrdersPage } from "./pages/OrdersPage";
+import { TrackingPage } from "./pages/TrackingPage";
 import { Route, Routes } from "react-router";
 import "./App.css";
 
@@ -11,6 +13,8 @@ function App() {
       {/* ↑ both top codes do same ↑ */}
 
       <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrdersPage />} />
+      <Route path="tracking" element={<TrackingPage />} />
     </Routes>
   );
 }
